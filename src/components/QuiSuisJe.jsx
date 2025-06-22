bb910465b9218fbcb4502a9994c3a45251e65902
Eric Pinto <eric.pinto@hotmail.fr>
2025-06-22 16:32:44 +0200

    init

--- src/components/QuiSuisJe.jsx
+++ src/components/QuiSuisJe.jsx
@@ -4,6 +4,7 @@ function QuiSuisJe() {
   return (
     <div>
       <h2>Qui suis je</h2>
+      <img src="/src/images/image.jpg" alt="Richard Gaglione" />
       <p>Présentation de Richard Gaglione.</p>
     </div>
   );
--- src/components/SoinsEnergetiques.jsx
+++ src/components/SoinsEnergetiques.jsx
@@ -4,6 +4,7 @@ function SoinsEnergetiques() {
   return (
     <div>
       <h2>Soins énergétiques</h2>
+      <img src="/src/images/healing-hands.png" alt="Soins énergétiques" />
       <p>Informations sur les soins énergétiques proposés.</p>
     </div>
   );
